26b69474c92210c22041818501e44e2e593559b6
Dmytro <ljutyjj.dima@gmail.com>
2019-10-30 20:43:02 +0200

    WIP - added router

--- whiteboard/server.js
+++ whiteboard/server.js
@@ -3,12 +3,17 @@ const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 
 const app = express();
+const router = express.Router();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
 }));
+
+router.route("/#").get((req, res) => {
+    console.log("GET " + req.params.id);
+});
 
 app.listen(3000, "0.0.0.0", function(){
     console.log("Example app listening on port 3000!\n");
