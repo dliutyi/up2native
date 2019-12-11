f1e52de2758b7d007d94a17f1956d20d01334ef1
Dmytro <ljutyjj.dima@gmail.com>
2019-12-12 00:25:08 +0200

    update

--- whiteboard/server.js
+++ whiteboard/server.js
@@ -25,9 +25,10 @@ server.listen(process.env.PORT || 4000, function(){
     console.log("Example app listening on port 3000!\n");
 });
 
-const url = "mongodb://user_up2native:db123456789@ds353738.mlab.com:53738";
-const params = { useUnifiedTopology: true, useNewUrlParser: true };
-mongo.connect(url, function(err, client){
+const url = "mongodb://heroku_gtnpx9mf:fcn6s9sk2tic8d8mj5pikaf2at@ds353738.mlab.com:53738/heroku_gtnpx9mf";
+const params = { useUnifiedTopology: true };
+mongo.connect(url, params, function(err, client){
+    console.log(err);
     console.log("connected");
     var db = client.db("heroku_gtnpx9mf");
     var sheets = db.collection("sheets");
