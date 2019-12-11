6053be7a2e282dc41bdcac1ef0bfd98fcf4a3869
Dmytro <ljutyjj.dima@gmail.com>
2019-12-11 23:41:20 +0200

    Added ignore

--- whiteboard/server.js
+++ whiteboard/server.js
@@ -25,54 +25,56 @@ server.listen(process.env.PORT || 4000, function(){
     console.log("Example app listening on port 3000!\n");
 });
 
-const url = "mongodb://user_up2native:db123456789@ds353738.mlab.com:53738/heroku_gtnpx9mf";
-mongo.connect(url, function(err, db){
+const url = "mongodb://user_up2native:db123456789@ds353738.mlab.com:53738";
+const params = { useUnifiedTopology: true, useNewUrlParser: true };
+mongo.connect(url, function(err, client){
     console.log("connected");
-    db.collection("sheets", function(err, sheets){
-        io.on("connection", function(socket){
-            console.log("user connected");
-        
-            socket.on("update", function(sheet){
-                const sid = sheet.id;
-                const sobjs = sheet.objs;
-                console.log("update " + sheet);
-                sheets.updateOne(
-                    {
-                        id: sid
-                    }, 
-                    { 
-                        $addToSet: { 
-                            objs: { 
-                                id: sobjs[0].id, 
-                                type: sobjs[0].type
-                            } 
-                        } 
-                    },  
-                    { upsert: true }
-                ).then(() => {
-                    sheets.updateOne(
-                    {
-                        id: sid, 
-                        objs: { $elemMatch: { id: sobjs[0].id } } 
-                    }, 
-                    { 
-                        $addToSet: { 
-                            "objs.$.deltas": { $each: sobjs[0].deltas }
-                        } 
-                    });
-                });
-                io.emit("update", sheet);
-            });
+    var db = client.db("heroku_gtnpx9mf");
+    var sheets = db.collection("sheets");
 
-            socket.on("initialize", function(sheet){
-                console.log("initialize " + sheet.id);
-                sheets.findOne({ 
-                    id: sheet.id
-                }).then((document) => {
-                    const sobjs = document.objs;
-                    io.emit("initialize", { id: sheet.id, objs: sobjs });
-                    console.log("state " + { id: sheet.id, objs: sobjs });
+    io.on("connection", function(socket){
+        console.log("user connected");
+    
+        socket.on("update", function(sheet){
+            const sid = sheet.id;
+            const sobjs = sheet.objs;
+            console.log("update " + sheet);
+            sheets.updateOne(
+                {
+                    id: sid
+                }, 
+                { 
+                    $addToSet: { 
+                        objs: { 
+                            id: sobjs[0].id, 
+                            type: sobjs[0].type
+                        } 
+                    } 
+                },  
+                { upsert: true }
+            ).then(() => {
+                sheets.updateOne(
+                {
+                    id: sid, 
+                    objs: { $elemMatch: { id: sobjs[0].id } } 
+                }, 
+                { 
+                    $addToSet: { 
+                        "objs.$.deltas": { $each: sobjs[0].deltas }
+                    } 
                 });
+            });
+            io.emit("update", sheet);
+        });
+
+        socket.on("initialize", function(sheet){
+            console.log("initialize " + sheet.id);
+            sheets.findOne({ 
+                id: sheet.id
+            }).then((document) => {
+                const sobjs = document.objs;
+                io.emit("initialize", { id: sheet.id, objs: sobjs });
+                console.log("state " + { id: sheet.id, objs: sobjs });
             });
         });
     
